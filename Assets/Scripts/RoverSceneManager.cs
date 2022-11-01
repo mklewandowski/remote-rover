76a266a232aeaccdbde9d278cea14a8160b81bad
mklewandowski <matt.kain.lewandowski@gmail.com>
2022-11-01 12:40:15 -0700

    Refactor scenemanager so it is easier to port to other projects

--- Assets/Scripts/RoverSceneManager.cs
+++ Assets/Scripts/RoverSceneManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class SceneManager : MonoBehaviour
+public class RoverSceneManager : MonoBehaviour
 {
     enum Directions {
         Up,
